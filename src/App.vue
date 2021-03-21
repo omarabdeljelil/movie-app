2060814cbd0f55daa929e0d2789d28d906859e8d
Omar Abdeljelil <omarabdeljelil@gmail.com>
2021-03-21 13:16:22 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </div>
-  <router-view/>
+  <router-view />
+  <!-- <div>
+    <input v-model="name" type="text" />
+    <br />
+    <p>
+      {{ name }}
+    </p>
+    <hr>
+    <button @click="incrementCounter" >increment </button>
+    {{counter}}
+
+  </div> -->
 </template>
+
+<script>
+// export default {
+//   data() {
+//     return {
+//       name: "omar",
+//       counter: 0
+//     };
+//   },
+//   methods: {
+//     incrementCounter(){
+//       this.counter++;
+//     }
+//   }
+// };
+</script>
 
 <style lang="scss">
 #app {
